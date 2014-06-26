34ec082c227847e03339b13d45c19e6468488461
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2014-06-26 01:10:46 -0300

    Fixed getEntityName method

--- src/AlecrimCoreData/AlecrimCoreData/Extensions/NSManagedObject+Extensions.swift
+++ src/AlecrimCoreData/AlecrimCoreData/Extensions/NSManagedObject+Extensions.swift
@@ -15,7 +15,7 @@ extension NSManagedObject {
     
     // TODO: change to class var
     class func getEntityName() -> String {
-        let className: NSString = NSStringFromClass(self.classForCoder())
+        let className: NSString = ___nameOfClass(self)
         
         //if className.hasSuffix(".Entity") {
         //    return className
@@ -31,4 +31,59 @@ extension NSManagedObject {
         }
     }
 
+}
+
+// from: https://github.com/indieSoftware/INSwift
+
+func ___nameOfClass(classType: AnyClass) -> String {
+    let stringOfClassType: String = NSStringFromClass(classType)
+    
+    // parse the returned string
+    let swiftClassPrefix = "_TtC"
+    if stringOfClassType.hasPrefix(swiftClassPrefix) {
+        // convert the string into an array for easyer access to the characters in it
+        let characters = Array(stringOfClassType)
+        var ciphersForModule = String()
+        // parse the ciphers for the module name's length
+        var index = countElements(swiftClassPrefix)
+        while index < characters.count {
+            let character = characters[index++]
+            if String(character).toInt() {
+                // character is a cipher
+                ciphersForModule += character
+            } else {
+                // no cipher, module name begins
+                break
+            }
+        }
+        // create a number from the ciphers
+        if let numberOfCharactersOfModuleName = ciphersForModule.toInt() {
+            // ciphers contains a valid number, so skip the module name minus 1 because we already read one character f the module name
+            index += numberOfCharactersOfModuleName - 1
+            var ciphersForClass = String()
+            while index < characters.count {
+                let character = characters[index++]
+                if String(character).toInt() {
+                    // character is a cipher
+                    ciphersForClass += character
+                } else {
+                    // no cipher, class name begins
+                    break
+                }
+            }
+            // create a number from the ciphers
+            if let numberOfCharactersOfClassName = ciphersForClass.toInt() {
+                // number parsed, but make sure it does not exceeds the string's length
+                if numberOfCharactersOfClassName > 0 && index - 1 + numberOfCharactersOfClassName <= characters.count {
+                    // valid number, get the substring which should be the classes' name
+                    let range = NSRange(location: index - 1, length: numberOfCharactersOfClassName)
+                    let nameOfClass = (stringOfClassType as NSString).substringWithRange(range)
+                    return nameOfClass
+                }
+            }
+        }
+    }
+    
+    // string couldn't be parsed so just return the returned string
+    return stringOfClassType
 }
