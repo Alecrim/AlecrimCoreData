cecc00a0eabc5b54c19149c6d996498ee4eb309d
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2014-08-05 17:01:40 -0300

    Fixed entityName in NSManagedObject extension

--- src/AlecrimCoreData/AlecrimCoreData-Shared/Extensions/NSManagedObject+Extensions.swift
+++ src/AlecrimCoreData/AlecrimCoreData-Shared/Extensions/NSManagedObject+Extensions.swift
@@ -9,6 +9,8 @@
 import Foundation
 import CoreData
 
+private var entityNames = Dictionary<String, String>()
+
 extension NSManagedObject {
     
     public func inDataModel(dataModel: CoreDataModel) -> Self? {
@@ -37,70 +39,29 @@ extension NSManagedObject {
 
 extension NSManagedObject {
     
-    internal class func getEntityName() -> String {
-        let className: NSString = ___nameOfClass(self)
-        let range = className.rangeOfString("Entity")
-        
-        if range.location == NSNotFound || range.location == 0 {
-            return className;
+    internal class var entityName: String {
+        let className = NSStringFromClass(self)
+            
+        if let name = entityNames[className] {
+            return name
         }
         else {
-            return className.substringToIndex(range.location)
+            var name: NSString = className
+            let range = name.rangeOfString(".")
+            if range.location != NSNotFound {
+                name = name.substringFromIndex(range.location + 1)
+            }
+            
+            let suffix: NSString = "Entity"
+            if name != suffix && name.hasSuffix(suffix) {
+                name = name.substringToIndex(name.length - suffix.length)
+            }
+            
+            entityNames[className] = name
+            
+            return name
         }
     }
 
 }
 
-// from: https://github.com/indieSoftware/INSwift
-private func ___nameOfClass(classType: AnyClass) -> String {
-    let stringOfClassType: String = NSStringFromClass(classType)
-    
-    // parse the returned string
-    let swiftClassPrefix = "_TtC"
-    if stringOfClassType.hasPrefix(swiftClassPrefix) {
-        // convert the string into an array for easyer access to the characters in it
-        let characters = Array(stringOfClassType)
-        var ciphersForModule = String()
-        // parse the ciphers for the module name's length
-        var index = countElements(swiftClassPrefix)
-        while index < characters.count {
-            let character = characters[index++]
-            if String(character).toInt() != nil {
-                // character is a cipher
-                ciphersForModule += character
-            } else {
-                // no cipher, module name begins
-                break
-            }
-        }
-        // create a number from the ciphers
-        if let numberOfCharactersOfModuleName = ciphersForModule.toInt() {
-            // ciphers contains a valid number, so skip the module name minus 1 because we already read one character f the module name
-            index += numberOfCharactersOfModuleName - 1
-            var ciphersForClass = String()
-            while index < characters.count {
-                let character = characters[index++]
-                if (String(character).toInt() != nil) {
-                    // character is a cipher
-                    ciphersForClass += character
-                } else {
-                    // no cipher, class name begins
-                    break
-                }
-            }
-            // create a number from the ciphers
-            if let numberOfCharactersOfClassName = ciphersForClass.toInt() {
-                // number parsed, but make sure it does not exceeds the string's length
-                if numberOfCharactersOfClassName > 0 && index - 1 + numberOfCharactersOfClassName <= characters.count {
-                    // valid number, get the substring which should be the classes' name
-                    let range = NSRange(location: index - 1, length: numberOfCharactersOfClassName)
-                    let nameOfClass = (stringOfClassType as NSString).substringWithRange(range)
-                    return nameOfClass
-                }
-            }
-        }
-    }
-    
-    // string couldn't be parsed so just return the returned string
-    return stringOfClassType
-}
