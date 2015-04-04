674c801a79aebdafd374a29492522af85a8cc614
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2015-04-04 20:01:14 -0300

    Updated examples

--- Examples/OS X/AlecrimCoreDataExample/ViewController.swift
+++ Examples/OS X/AlecrimCoreDataExample/ViewController.swift
@@ -34,59 +34,29 @@ class ViewController: NSViewController {
     }
     
     func add(sender: AnyObject) {
-        // Insert new entity using background data context for example purpose (it would be inserted using the main thread data context too).
-        performInBackground(dataContext) { backgroundDataContext in
-            let newEventEntity = backgroundDataContext.events.createEntity()
-            
-            // Configure the new managed object.
-            newEventEntity.timeStamp = NSDate()
-            
-            // Save the background data context.
-            let (success, error) = backgroundDataContext.save()
-            if !success {
-                // Replace this implementation with code to handle the error appropriately.
-                println("Unresolved error \(error), \(error?.userInfo)")
-            }
+        let newEventEntity = dataContext.events.createEntity()
+
+        // Configure the new managed object.
+        newEventEntity.timeStamp = NSDate()
+
+        // Save the background data context.
+        let (success, error) = dataContext.save()
+        if !success {
+            // Replace this implementation with code to handle the error appropriately.
+            println("Unresolved error \(error), \(error?.userInfo)")
         }
-        
-//        // OR insert new entity using main data context.
-//        let newEventEntity = dataContext.events.createEntity()
-//
-//        // Configure the new managed object.
-//        newEventEntity.timeStamp = NSDate()
-//
-//        // Save the background data context.
-//        let (success, error) = dataContext.save()
-//        if !success {
-//            // Replace this implementation with code to handle the error appropriately.
-//            println("Unresolved error \(error), \(error?.userInfo)")
-//        }
     }
     
     func remove(sender: AnyObject) {
         if let entity = self.arrayController.selectedObjects.first as? EventEntity {
-            performInBackground(dataContext) { backgroundDataContext in
-                let backgroundEntity = entity.inContext(backgroundDataContext)
-                backgroundDataContext.events.deleteEntity(backgroundEntity!)
-                
-                if !backgroundDataContext.save() {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    abort()
-                }
+            dataContext.events.deleteEntity(entity)
+            
+            if !dataContext.save().0 {
+                // Replace this implementation with code to handle the error appropriately.
+                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                abort()
             }
         }
-
-//        // OR delete entity using main thread data context.
-//        if let entity = self.arrayController.selectedObjects.first as? EventEntity {
-//            dataContext.events.deleteEntity(entity)
-//            
-//            if !dataContext.save() {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                abort()
-//            }
-//        }
     }
     
 }
