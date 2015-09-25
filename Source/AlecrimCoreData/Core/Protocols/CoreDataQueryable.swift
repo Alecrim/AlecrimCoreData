3c3f91bcd76b3acc5c915ac4f6c11ffa600e34bb
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2015-09-25 18:12:13 -0300

    Added back aggregate methods; see #85

--- Source/AlecrimCoreData/Core/Protocols/CoreDataQueryable.swift
+++ Source/AlecrimCoreData/Core/Protocols/CoreDataQueryable.swift
@@ -42,3 +42,59 @@ extension CoreDataQueryable {
     }
     
 }
+
+
+// MARK: - aggregate
+
+extension CoreDataQueryable {
+    
+    public func sum<U>(attributeClosure: (Self.Item.Type) -> Attribute<U>) -> U {
+        return self.aggregateWithFunctionName("sum", attributeClosure: attributeClosure)
+    }
+    
+    public func min<U>(attributeClosure: (Self.Item.Type) -> Attribute<U>) -> U {
+        return self.aggregateWithFunctionName("min", attributeClosure: attributeClosure)
+    }
+    
+    public func max<U>(attributeClosure: (Self.Item.Type) -> Attribute<U>) -> U {
+        return self.aggregateWithFunctionName("max", attributeClosure: attributeClosure)
+    }
+
+    // same as average, for convenience
+    public func avg<U>(attributeClosure: (Self.Item.Type) -> Attribute<U>) -> U {
+        return self.aggregateWithFunctionName("average", attributeClosure: attributeClosure)
+    }
+
+    public func average<U>(attributeClosure: (Self.Item.Type) -> Attribute<U>) -> U {
+        return self.aggregateWithFunctionName("average", attributeClosure: attributeClosure)
+    }
+    
+    private func aggregateWithFunctionName<U>(functionName: String, @noescape attributeClosure: (Self.Item.Type) -> Attribute<U>) -> U {
+        let attribute = attributeClosure(Self.Item.self)
+        let attributeDescription = self.entityDescription.attributesByName[attribute.___name]!
+        
+        let keyPathExpression = NSExpression(forKeyPath: attribute.___name)
+        let functionExpression = NSExpression(forFunction: "\(functionName):", arguments: [keyPathExpression])
+        
+        let expressionDescription = NSExpressionDescription()
+        expressionDescription.name = "___\(functionName)"
+        expressionDescription.expression = functionExpression
+        expressionDescription.expressionResultType = attributeDescription.attributeType
+        
+        let fetchRequest = self.toFetchRequest()
+        fetchRequest.propertiesToFetch =  [expressionDescription]
+        fetchRequest.resultType = NSFetchRequestResultType.DictionaryResultType
+        
+        let results = try! self.dataContext.executeFetchRequest(fetchRequest)
+        
+        let value: AnyObject = (results.first as! NSDictionary).valueForKey(expressionDescription.name)!
+        if let safeValue = value as? U {
+            return safeValue
+        }
+        else {
+            // HAX: try brute force
+            return unsafeBitCast(value, U.self)
+        }
+    }
+    
+}
